693a842f5caa967ac4ade21501c36cf352999f57
jaydip-changani <pjaydip956@gmail.com>
2025-08-29 18:53:59 +0530

    style changes and header added

---
+++ app/header.js
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header className="text-black text-center flex itmes-space-around p-5 justify-between">
+      <h1 className="text-2xl font-bold">Jaydip Changani</h1>
+    
+        <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition">
+        Collaborate
+        </button>
+            
+    </header>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,30 +1,36 @@
 "use client";
 import { motion } from "framer-motion";import Image from "next/image";
-
+import Header from "./header";
 //use popins
 export default function Home() {
   return (
     <>
     <div>
       
+      
       <motion.div
-      className="min-h-screen flex items-center justify-center m-5 rounded-lg font-sans"
+      className="min-h-screen m-5 rounded-lg font-sans"
       animate={{
         backgroundColor: ["#ADBAE8", "#ECE1ED" ,"#e6dbdbff"],
       }}
       transition={{ duration: 10, repeat: Infinity, repeatType: "reverse" }}
     >
-      <div>
-<p className="text-2xl ">Hi, I'm Jaydip, a design-driven</p>
-      <h1 className=" text-8xl ">full-stack </h1><br/>
-      <h1 className=" text-9xl"> developer</h1>
-      <div className="text-1xl flex items-end justify-end">
+      <Header />
+      <div className="flex items-center justify-center p-60"> 
+
+
+      <div >
+<p className="text-lg leading-loose">Hi, I'm Jaydip, a design-driven</p>
+      <p className=" text-7xl font-medium">full-stack </p><br/>
+      <p className=" text-8xl font-normal"> developer</p>
+      <div className="text flex items-end justify-end">
 
       
-      <h3 className="text-2xl">  Craft. Refine. Innovate. Solve. </h3>
+      <h3 className="text-lg leading-loose">  Craft. Refine. Innovate. Solve. </h3>
 </div>
       </div>
       
+      </div>
     </motion.div>
     </div>
     </>
