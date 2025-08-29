5f50638a730bbf27aa63538390f4485bdd1e3f2c
jaydip-changani <pjaydip956@gmail.com>
2025-08-29 18:13:34 +0530

    background with effect

--- app/page.js
+++ app/page.js
@@ -1,13 +1,33 @@
-import Image from "next/image";
+"use client";
+import { motion } from "framer-motion";import Image from "next/image";
 
+//use popins
 export default function Home() {
   return (
+    <>
     <div>
-      <h1 className="text-3xl font-bold underline ">Hello, Next.js!</h1>
+      
+      <motion.div
+      className="min-h-screen flex items-center justify-center m-5 rounded-lg font-sans"
+      animate={{
+        backgroundColor: ["#ADBAE8", "#ECE1ED" ,"#e6dbdbff"],
+      }}
+      transition={{ duration: 10, repeat: Infinity, repeatType: "reverse" }}
+    >
+      <div>
+<p className="text-2xl ">Hi, I'm Jaydip, a design-driven</p>
+      <h1 className=" text-8xl ">full-stack </h1><br/>
+      <h1 className=" text-9xl"> developer</h1>
+      <div className="text-1xl flex items-end justify-end">
 
-      <p className="text-red-500">This is a red text.</p>
-      <p className="text-green-500">This is a green text.</p>
-
+      
+      <h3 className="text-2xl">  Craft. Refine. Innovate. Solve. </h3>
+</div>
+      </div>
+      
+    </motion.div>
     </div>
+    </>
+     
   );
 }
