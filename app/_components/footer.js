a3be1e420e02c0daa5bf2ccfb224d2b76990e25c
jaydip-changani <pjaydip956@gmail.com>
2025-09-03 17:52:01 +0530

    add Head fro SEO

--- app/_components/footer.js
+++ app/_components/footer.js
@@ -2,33 +2,32 @@ import Link from "next/link";
 
 export default function Footer() {
     return (
-        <footer className="bg-gray-800 text-[#F9FAFB] mx-5 rounded-2xl flex justify-center py-20">
-            <div className="container flex flex-col md:flex-row gap-10 md:gap-0 items-start px-4">
-                {/* Left Side */}
+        <footer aria-label="Footer" className="bg-gray-800 text-[#F9FAFB] mx-5 rounded-2xl flex justify-center py-20">
+            <div className="container flex flex-col md:flex-row gap-10 md:gap-0 items-start px-4 w-full">
+
                 <div className="w-full md:w-1/2 space-y-3">
                     <h1 className="text-3xl">Let&#39;s build something together.</h1>
                     <p className="text-sm text-[#9CA3AF]">
                         Feel free to reach out if you&#39;ve a question, or just want to connect.
                     </p>
                     <h2 className="text-xl py-6 md:py-15">jaydipchangani2@gmail.com</h2>
-                    <p className="text-sm text-[#9CA3AF]">
+
+                    <p className="text-sm text-[#9CA3AF] hidden md:block">
                         ©2025 Jaydip Changani - Built with Next.js and TailwindCSS
                     </p>
                 </div>
 
-                {/* Right Side (Links) */}
                 <div className="w-full md:w-1/2 flex flex-col md:flex-row gap-8">
-                    {/* Column 1 */}
+
                     <div className="w-full md:w-1/3 space-y-3">
                         <p className="font-bold">Find me on</p>
-                        <ol className="space-y-3 text-sm text-[#9CA3AF]">
+                        <ul className="space-y-3 text-sm text-[#9CA3AF]">
                             <li><Link href="https://www.linkedin.com/in/jaydip-changani">LinkedIn</Link></li>
                             <li><Link href="https://github.com/jaydipchangani">GitHub</Link></li>
                             <li><Link href="https://www.instagram.com/jaydip_changani">Instagram</Link></li>
-                        </ol>
+                        </ul>
                     </div>
 
-                    {/* Column 2 */}
                     <div className="w-full md:w-1/3 space-y-2">
                         <p className="font-bold">Personalia</p>
                         <ol className="space-y-2 text-sm text-[#9CA3AF]">
@@ -37,7 +36,6 @@ export default function Footer() {
                         </ol>
                     </div>
 
-                    {/* Column 3 */}
                     <div className="w-full md:w-1/3 space-y-2 ">
                         <p className="font-bold">Site Map</p>
                         <ol className="space-y-2 text-sm text-[#9CA3AF]">
@@ -46,6 +44,10 @@ export default function Footer() {
                         </ol>
                     </div>
                 </div>
+
+                <p className="text-sm text-[#9CA3AF] mt-10 md:hidden text-center w-full">
+                    ©2025 Jaydip Changani - Built with Next.js and TailwindCSS
+                </p>
             </div>
         </footer>
     );
--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import Header from "./_components/header";
 import NavBar from "./_components/navBar";
 import Info from "./_components/info";
 import ProjectCard from "./_components/projectCard";
+import Head from "next/head";
 
 const fadeUp = {
   initial: { opacity: 0, y: 20 },
@@ -14,6 +15,34 @@ const fadeUp = {
 export default function Home() {
   return (
     <>
+    <Head>
+        <title>Jaydip Changani - Full-stack Developer Portfolio</title>
+        <meta
+          name="description"
+          content="Hi, I'm Jaydip, a design-driven full-stack developer. Explore my portfolio showcasing projects, skills, and innovative solutions."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <meta property="og:title" content="Jaydip Changani - Full-stack Developer Portfolio" />
+        <meta
+          property="og:description"
+          content="Explore the portfolio of Jaydip, a full-stack developer focused on crafting, refining, and innovating solutions."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://jaydip-next-js-portfolio.vercel.app/" />
+        <meta property="og:image" content="https://jaydip-next-js-portfolio.vercel.app/_next/image?url=%2FJaydipImage.jpeg&w=1920&q=75" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Jaydip Changani - Full-stack Developer Portfolio" />
+        <meta
+          name="twitter:description"
+          content="Hi, I'm Jaydip, a full-stack developer. Check out my projects and skills portfolio."
+        />
+        <meta name="twitter:image" content="https://jaydip-next-js-portfolio.vercel.app/" />
+
+        <link rel="canonical" href="https://jaydip-next-js-portfolio.vercel.app/" />
+      </Head>
+
       <div>
         <motion.div
           className="min-h-screen m-5 rounded-4xl"
